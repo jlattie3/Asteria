75be1dd840b2c58affbc32e8824cba8eebb7d01e
Jacob Lattie <jhlattie@gmail.com>
2022-08-23 17:50:19 -0400

    Improve demo app

--- AsteriaDemo/AsteriaDemo/ViewController.swift
+++ AsteriaDemo/AsteriaDemo/ViewController.swift
@@ -10,12 +10,48 @@ import Asteria
 
 class ViewController: UIViewController {
 
+    private let scrollView = UIScrollView()
+    private let stackView = UIStackView()
+    private var label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Loading..."
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        navigationItem.title = "Asteria"
+        navigationItem.rightBarButtonItem = .init(image: .init(systemName: "flame"), style: .plain, target: self, action: #selector(fetchAPOD))
+
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        scrollView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+
+        scrollView.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 0).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: 0).isActive = true
+        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 0).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0).isActive = true
+        stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+
+        stackView.addArrangedSubview(label)
+        fetchAPOD()
+    }
+
+    @objc
+    private func fetchAPOD() {
         Task {
             do {
                 let apod = try await Asteria().apodService.getAPOD(with: .random(count: 1))
+                label.text = "\(apod)"
                 print(apod)
             } catch {
                 print(error.localizedDescription)
